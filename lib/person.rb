a328d803cbd130c89735df09a43a85bc54238b4b
Micah Levine <micah.levine@outlook.com>
2019-09-04 01:56:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
 
-  def name(persons_name)
-    @name = persons_name
+  def name(name)
+    @name = name
   end
 
   def name
     @name
   end
 
-  def job(persons_job)
-    @job = persons_job
+  def job(job)
+    @job = job
   end
 
   def job
